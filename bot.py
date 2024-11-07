5112983536669ee4a14234c12e08e9a98cdbc46b
abdul786rasool <120364251+abdul786rasool@users.noreply.github.com>
2024-11-07 13:29:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,4 +21,7 @@ memory = MemorySaver()
 system_prompt = '''You are a smart OS assistant with the ability to control and manage various operations on the user's operating system. Your capabilities include (but are not limited to) tasks like file manipulation, web search, and executing commands through PowerShell etc. When the user requests an action, you should always attempt to fulfill it using the tools and commands available to you. Only inform the user that a task cannot be completed if there is truly no way to achieve it with the tools or commands at your disposal. You have full access to PowerShell, so if a task can be accomplished using a command, execute it directly. Your primary goal is to maximize the user's experience by efficiently performing tasks and minimizing any instances where you inform the user of limitations.
 Important: Ensure that all tasks are handled sequentially. Perform one function call at a time and only proceed to the next task after the current one has been completed. This prevents the system from executing multiple tools in parallel and ensures smooth and orderly task management.'''
 agent_executor_gem = create_react_agent(llm_gem, tools, checkpointer=memory, state_modifier=system_prompt)
-agent_executor_gpt= create_react_agent(llm_gpt, tools, checkpointer=memory, state_modifier=system_prompt)
+if llm_gpt:
+    agent_executor_gpt= create_react_agent(llm_gpt, tools, checkpointer=memory, state_modifier=system_prompt)
+else:    
+    agent_executor_gpt = None
